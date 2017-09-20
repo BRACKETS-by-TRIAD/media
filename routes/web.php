8c4305c55fa568e9ead0b0e79e38814981f4067e
David Běhal <behal.david@gmail.com>
2017-09-20 16:58:50 +0200

    Changed named routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 Route::middleware(['web'])->group(function () {
     Route::namespace('Brackets\Media\Http\Controllers')->group(function () {
-        Route::post('upload',                   'FileUploadController@upload')->name('brackets/media:upload');
-        Route::get('view',                      'FileViewController@view')->name('brackets/media:view');
+        Route::post('upload',                   'FileUploadController@upload')->name('brackets/media::upload');
+        Route::get('view',                      'FileViewController@view')->name('brackets/media::view');
     });
 });
--- src/UrlGenerator/LocalUrlGenerator.php
+++ src/UrlGenerator/LocalUrlGenerator.php
@@ -9,7 +9,7 @@ class LocalUrlGenerator extends SpatieLocalUrlGenerator {
     public function getUrl(): string {
         if($this->media->disk == 'media_private') {
             $url = $this->getPathRelativeToRoot();
-            return route('brackets/media:view', [], false) . '?path=' . $this->makeCompatibleForNonUnixHosts($url);
+            return route('brackets/media::view', [], false) . '?path=' . $this->makeCompatibleForNonUnixHosts($url);
         } else {
             $url = $this->getBaseMediaDirectory().'/'.$this->getPathRelativeToRoot();
             return $this->makeCompatibleForNonUnixHosts($url);
